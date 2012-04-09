e97e3eee43f4c24bcd13fee58202358389f8958c
Chris Johnsen <chris_johnsen@pobox.com>
2012-04-09 02:36:35 -0500

    Switch to a window-level capture handler
    Some bookmarklets are hosted on pages that bind a capturing event
    listener to a higher-level element to prevent the accidental use of
    the bookmarklet on its hosting page.
    
    Move all of our click handling into a single capturing handler on
    the window object (the highest level).
    
    This should allow us to usurp any capturing handlers installed at
    a *lower* level. There is nothing we can do if there is a prior
    window-level capturing handler that stops propagation (it will run
    first and prevent our handler from running).

--- view-bookmarklets-bookmarklet.js
+++ view-bookmarklets-bookmarklet.js
@@ -53,99 +53,82 @@
 enableBookmarkletViewing = (function() {
     'use strict';
 
-    function forEachJavaScriptAnchor(func) {
-        var i, anchorList = document.getElementsByTagName('a');
-        for(i = 0; i < anchorList.length; i++) {
-            if (anchorList[i].href.search(/^javascript:/) !== -1) {
-                func(anchorList[i]);
-            }
+    var style, overlay = document.createElement('div');
+    style = overlay.style;
+    style.font = '12px sans-serif';
+    style.position = 'fixed';
+    style.top = style.bottom = style.right = style.left = '0';
+    style.background = '#bbb';
+    style.color = '#000';
+    style.display = 'none';
+    function showOverlay(e) {
+        e.stopPropagation();
+        e.preventDefault();
+        overlay.style.display = 'block';
+    }
+    function hideOverlay(e) {
+        e.stopPropagation();
+        e.preventDefault();
+        overlay.style.display = 'none';
+    }
+    // iOS 4.3 (at least in the simulator) seems to "optimize away"
+    // the window-level click handler on everything except the
+    // disableLink (i.e. no way to dismiss without also disabling)
+    // unless we do something like the following:
+    overlay.addEventListener('click', function(e) {}, false);
+
+    var container = document.createElement('div');
+    style = container.style;
+    style.background = '#eee';
+    style.margin = '50px';
+    style.border = '5px solid #999';
+    style['border-radius'] = '5px';
+    style.padding = '1em';
+
+    var instructionContainer = document.createElement('div');
+    instructionContainer.style['font-size'] = '20px';
+
+    var disableContainer = document.createElement('div');
+    var disableLink = document.createElement('a');
+    disableLink.textContent =
+        'Tap or click here to disable bookmarklet viewing.';
+    disableLink.href = '#';
+
+    var returnText = document.createElement('div');
+    returnText.style['margin-bottom'] = '5px';
+    returnText.textContent =
+        'Tap or click outside to return to page.';
+
+    var displayTextarea = document.createElement('textarea');
+    style = displayTextarea.style;
+    style.width = '100%';
+    style.height = '70%';
+    displayTextarea.id = 'vbb_textarea';
+
+    disableContainer.appendChild(disableLink);
+    instructionContainer.appendChild(disableContainer);
+    instructionContainer.appendChild(returnText);
+    container.appendChild(instructionContainer);
+    container.appendChild(displayTextarea);
+    overlay.appendChild(container);
+    document.getElementsByTagName('body')[0].
+        appendChild(overlay);
+
+    function handleClick(e) {
+        if (e.target === overlay) {
+            hideOverlay(e);
+        } else if (e.target === disableLink) {
+            window.removeEventListener('click', handleClick, true);
+            hideOverlay(e);
+        } else if (e.target.tagName === 'A' &&
+                   /^javascript:/.test(e.target.href)) {
+            displayTextarea.value = unescape(e.target.href);
+            showOverlay(e);
+            displayTextarea.select();
         }
     }
 
-    var displayTextarea;
-
-    var displayBookmarkletSource =
-        document.getElementById('vbb_textarea') ?
-        document.getElementById('vbb_textarea').vbb_dbs :
-        (function() {
-        var style, overlay = document.createElement('div');
-        style = overlay.style;
-        style.font = '12px sans-serif';
-        style.position = 'fixed';
-        style.top = style.bottom = style.right = style.left = '0';
-        style.background = '#bbb';
-        style.color = '#000';
-        style.display = 'none';
-        function showOverlay(e) {
-            e.stopPropagation();
-            e.preventDefault();
-            overlay.style.display = 'block';
-        }
-        function hideOverlay(e) {
-            e.stopPropagation();
-            e.preventDefault();
-            overlay.style.display = 'none';
-        }
-        overlay.addEventListener('click', hideOverlay, false);
-
-        var container = document.createElement('div');
-        style = container.style;
-        style.background = '#eee';
-        style.margin = '50px';
-        style.border = '5px solid #999';
-        style['border-radius'] = '5px';
-        style.padding = '1em';
-        container.addEventListener('click',
-                function(e) { e.stopPropagation(); },
-                false);
-
-        var instructionContainer = document.createElement('div');
-        instructionContainer.style['font-size'] = '20px';
-
-        var disableContainer = document.createElement('div');
-        var disableLink = document.createElement('a');
-        disableLink.textContent =
-            'Tap or click here to disable bookmarklet viewing.';
-        function disableViewing(e) {
-            forEachJavaScriptAnchor(function(a) {
-                a.removeEventListener('click',
-                    displayBookmarkletSource, false);
-            });
-            hideOverlay(e);
-        }
-        disableLink.addEventListener('click', disableViewing, false);
-        disableLink.href = '#';
-
-        var returnText = document.createElement('div');
-        returnText.style['margin-bottom'] = '5px';
-        returnText.textContent =
-            'Tap or click outside to return to page.';
-
-        var displayTextarea = document.createElement('textarea');
-        style = displayTextarea.style;
-        style.width = '100%';
-        style.height = '70%';
-        displayTextarea.id = 'vbb_textarea';
-        displayTextarea.vbb_dbs = function(e) {
-            displayTextarea.value = unescape(this.href);
-            showOverlay(e);
-            displayTextarea.select();
-        };
-
-        disableContainer.appendChild(disableLink);
-        instructionContainer.appendChild(disableContainer);
-        instructionContainer.appendChild(returnText);
-        container.appendChild(instructionContainer);
-        container.appendChild(displayTextarea);
-        overlay.appendChild(container);
-        document.getElementsByTagName('body')[0].
-            appendChild(overlay);
-        return displayTextarea.vbb_dbs;
-    }());
-
     return function() {
-        forEachJavaScriptAnchor(function(a) {
-            a.addEventListener('click', displayBookmarkletSource, false);
-        });
+        window.addEventListener('click', handleClick, true);
     };
 }());
